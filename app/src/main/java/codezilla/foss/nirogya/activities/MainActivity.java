94cf8e652781d5fe47ac85021d9547b5ea8fae9f
Chinthaka <kasunchinthaka555@gmail.com>
2018-05-11 17:36:08 +0530

    first commit

---
+++ app/src/main/java/codezilla/foss/nirogya/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package codezilla.foss.nirogya.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import codezilla.foss.nirogya.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/codezilla/foss/nirogya/activities/SplashScreenActivity.java
@@ -0,0 +1,34 @@
+package codezilla.foss.nirogya.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import codezilla.foss.nirogya.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh_screen);
+
+        new Handler().postDelayed(new Runnable() {
+			/*
+			 * Showing splash screen with a timer.
+			 */
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
