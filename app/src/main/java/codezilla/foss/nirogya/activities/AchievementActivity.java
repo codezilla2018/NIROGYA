26171985255d9af946d4a45162cfbbd1fda98ecb
Chinthaka <kasunchinthaka555@gmail.com>
2018-05-23 14:00:41 +0530

    adding basic funtionalities of a decoview

--- app/src/main/java/codezilla/foss/nirogya/activities/AchievementActivity.java
+++ app/src/main/java/codezilla/foss/nirogya/activities/AchievementActivity.java
@@ -1,13 +1,20 @@
 package codezilla.foss.nirogya.activities;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AnticipateInterpolator;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.hookedonplay.decoviewlib.DecoView;
+import com.hookedonplay.decoviewlib.charts.DecoDrawEffect;
+import com.hookedonplay.decoviewlib.charts.SeriesItem;
+import com.hookedonplay.decoviewlib.events.DecoEvent;
 
 import codezilla.foss.nirogya.R;
 
@@ -17,6 +24,12 @@ public class AchievementActivity extends AppCompatActivity {
     private Button bronzeMedal, silverMedal, goldMedal;
     private LinearLayout silverMedalLayout;
     private TextView targetAchievementPoints;
+    private int mBackIndex;
+    private int mSeries1Index;
+    private DecoView mDecoView;
+    private final float mSeriesMax = 10000f;
+    private int mSeries2Index;
+    private int mSeries3Index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +42,13 @@ public class AchievementActivity extends AppCompatActivity {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(this.getResources().getColor(R.color.status_bar_color));
         }
+
+        mDecoView = findViewById(R.id.dynamicArcView);
+
+        // Create required data series on the DecoView
+        createBackSeries();
+        createDataSeries1();
+        createEvents();
         targetAcheivement = findViewById(R.id.targetAchievement);
         targetAchievementPoints = findViewById(R.id.targetAchievementPoints);
         bronzeMedal = findViewById(R.id.bronzeMedal);
@@ -53,5 +73,152 @@ public class AchievementActivity extends AppCompatActivity {
                 targetAchievementPoints.setText(getResources().getString(R.string.achievement_activity_target_point_3));
             }
         });
+    }
+    private void createBackSeries() {
+        SeriesItem seriesItem = new SeriesItem.Builder(Color.parseColor("#FFE2E2E2"))
+                .setRange(0, mSeriesMax, 0)
+                .setInitialVisibility(true)
+                .build();
+
+        mBackIndex = mDecoView.addSeries(seriesItem);
+    }
+
+    private void createDataSeries1() {
+        final SeriesItem seriesItem = new SeriesItem.Builder(Color.parseColor("#FFFF8800"))
+                .setRange(0, mSeriesMax, 0)
+                .setInitialVisibility(false)
+                .build();
+
+        final TextView textPercentage = (TextView) findViewById(R.id.textPercentage);
+        seriesItem.addArcSeriesItemListener(new SeriesItem.SeriesItemListener() {
+            @Override
+            public void onSeriesItemAnimationProgress(float percentComplete, float currentPosition) {
+                float percentFilled = ((currentPosition - seriesItem.getMinValue()) / (seriesItem.getMaxValue() - seriesItem.getMinValue()));
+                textPercentage.setText(String.format("%.0f%%", percentFilled * 100f));
+            }
+
+            @Override
+            public void onSeriesItemDisplayProgress(float percentComplete) {
+
+            }
+        });
+
+
+        final TextView textToGo = (TextView) findViewById(R.id.textRemaining);
+        seriesItem.addArcSeriesItemListener(new SeriesItem.SeriesItemListener() {
+            @Override
+            public void onSeriesItemAnimationProgress(float percentComplete, float currentPosition) {
+                textToGo.setText(String.format("%.1f steps to goal", seriesItem.getMaxValue() - currentPosition));
+
+            }
+
+            @Override
+            public void onSeriesItemDisplayProgress(float percentComplete) {
+
+            }
+        });
+
+        //  final TextView textActivity1 = (TextView) findViewById(R.id.textActivity1);
+        seriesItem.addArcSeriesItemListener(new SeriesItem.SeriesItemListener() {
+            @Override
+            public void onSeriesItemAnimationProgress(float percentComplete, float currentPosition) {
+                //  textActivity1.setText(String.format("%.0f Km", currentPosition));
+            }
+
+            @Override
+            public void onSeriesItemDisplayProgress(float percentComplete) {
+
+            }
+        });
+
+        mSeries1Index = mDecoView.addSeries(seriesItem);
+    }
+    private void createEvents() {
+        mDecoView.executeReset();
+
+        mDecoView.addEvent(new DecoEvent.Builder(mSeriesMax)
+                .setIndex(mBackIndex)
+                .setDuration(3000)
+                .setDelay(100)
+                .build());
+
+        mDecoView.addEvent(new DecoEvent.Builder(DecoDrawEffect.EffectType.EFFECT_SPIRAL_OUT)
+                .setIndex(mSeries1Index)
+                .setDuration(2000)
+                .setDelay(1250)
+                .build());
+
+        mDecoView.addEvent(new DecoEvent.Builder(6000.4f)
+                .setIndex(mSeries1Index)
+                .setDelay(3250)
+                .build());
+
+        mDecoView.addEvent(new DecoEvent.Builder(DecoDrawEffect.EffectType.EFFECT_SPIRAL_OUT)
+                .setIndex(mSeries2Index)
+                .setDuration(1000)
+                .setEffectRotations(1)
+                .setDelay(7000)
+                .build());
+
+        mDecoView.addEvent(new DecoEvent.Builder(16.3f)
+                .setIndex(mSeries2Index)
+                .setDelay(8500)
+                .build());
+
+        mDecoView.addEvent(new DecoEvent.Builder(DecoDrawEffect.EffectType.EFFECT_SPIRAL_OUT)
+                .setIndex(mSeries3Index)
+                .setDuration(1000)
+                .setEffectRotations(1)
+                .setDelay(12500)
+                .build());
+
+        mDecoView.addEvent(new DecoEvent.Builder(4.36f).setIndex(mSeries3Index).setDelay(14000).build());
+
+        mDecoView.addEvent(new DecoEvent.Builder(0).setIndex(mSeries3Index).setDelay(18000).build());
+
+        mDecoView.addEvent(new DecoEvent.Builder(0).setIndex(mSeries2Index).setDelay(18000).build());
+
+        mDecoView.addEvent(new DecoEvent.Builder(0)
+                .setIndex(mSeries1Index)
+                .setDelay(20000)
+                .setDuration(1000)
+                .setInterpolator(new AnticipateInterpolator())
+                .setListener(new DecoEvent.ExecuteEventListener() {
+                    @Override
+                    public void onEventStart(DecoEvent decoEvent) {
+
+                    }
+
+                    @Override
+                    public void onEventEnd(DecoEvent decoEvent) {
+                        resetText();
+                    }
+                })
+                .build());
+
+        mDecoView.addEvent(new DecoEvent.Builder(DecoDrawEffect.EffectType.EFFECT_SPIRAL_EXPLODE)
+                .setIndex(mSeries1Index)
+                .setDelay(21000)
+                .setDuration(3000)
+                .setDisplayText("GOAL!")
+                .setListener(new DecoEvent.ExecuteEventListener() {
+                    @Override
+                    public void onEventStart(DecoEvent decoEvent) {
+
+                    }
+
+                    @Override
+                    public void onEventEnd(DecoEvent decoEvent) {
+                        createEvents();
+                    }
+                })
+                .build());
+
+        resetText();
+    }
+
+    private void resetText() {
+        ((TextView) findViewById(R.id.textPercentage)).setText("");
+        ((TextView) findViewById(R.id.textRemaining)).setText("");
     }
 }
